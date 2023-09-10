7332224f7848d6d5529c33288e648ec642911584
faheemshams <faheemshams58@gmail.com>
2023-09-10 18:05:25 +0530

    aa

--- frontend/Javascript/main.js
+++ frontend/Javascript/main.js
@@ -6,12 +6,17 @@ const saveButton = document.getElementById('save-page-btn');
 const pageTitleInput = document.getElementById('page-title');
 const pageText= document.getElementById('page-text');
 const taskList = document.getElementById('task-list');
+const rightPane = document.getElementById('right-pane');
+const taskDetails = document.getElementById('task-details');
+let selectedTask = null;
 
-/*----------------------------------------------------------------------------------*/
 
 addTaskButton.addEventListener('click', ()=>
 {
+    taskDetails.innerHTML = '';
     pageModal.style.display = 'block';
+    const taskDetailsTitle = document.getElementById('task-details-titles');
+    taskDetailsTitle.textContent = "Add New Task";
 });
 
 saveButton.addEventListener('click', (e)=>
@@ -20,10 +25,11 @@ saveButton.addEventListener('click', (e)=>
     const title = pageTitleInput.value;
     const text = pageText.value;
  
-    const task = 
+    const task =
     {
         title : title,
-        text : text
+        text : text,
+        subTask : []
     };
 
     let existingTasks = JSON.parse(localStorage.getItem('tasks')) || [];
@@ -33,7 +39,6 @@ saveButton.addEventListener('click', (e)=>
     pageModal.style.display = 'none';
     pageTitleInput.value = '';
     quill.root.innerHTML = '';
-
     renderStoredTasks();
 });
 
@@ -45,26 +50,25 @@ function renderStoredTasks()
     const existingTasks = JSON.parse(localStorage.getItem('tasks')) || [];
     existingTasks.forEach((task, index) => 
     {
-        const listItem = document.createElement('li');
-        listItem.classList.add('new-task');
-        listItem.textContent = task.title;
+        const sublistlistItem = document.createElement('li');
+        sublistlistItem.classList.add('new-task');
+        sublistlistItem.textContent = task.title;
 
-        listItem.addEventListener('click', () => 
+        sublistlistItem.addEventListener('click', () => 
         {
             renderTaskDetails(task);
         });
 
         const deleteButton = document.createElement('button');
         deleteButton.textContent = 'Delete';
-        deleteButton.style.background = '#eb7b7b';
+        deleteButton.classList.add('delete-button');
         
-        listItem.appendChild(deleteButton);
-        taskList.appendChild(listItem);
+        sublistlistItem.appendChild(deleteButton);
+        taskList.appendChild(sublistlistItem);
     });
 }
 
-function renderTaskDetails(task)
-{
+function renderTaskDetails(task) {
     const taskDetails = document.getElementById('task-details');
     taskDetails.innerHTML = '';
 
@@ -73,24 +77,102 @@ function renderTaskDetails(task)
 
     const textBody = document.createElement('p');
     textBody.textContent = task.text;
-    textBody.style.marginTop = '30px'
+    textBody.style.marginTop = '30px';
     textBody.style.marginBottom = '20px';
 
     const editButton = document.createElement('button');
     editButton.textContent = 'Edit';
 
-    const subPageButton = document.createElement('button');
-    subPageButton.textContent = 'Add Subtask';
-    subPageButton.style.width = '40px'
+    const addSubTaskButton = document.createElement('button');
+    addSubTaskButton.textContent = 'Add Subtask';
 
-    
+    addSubTaskButton.addEventListener('click', () => {
+        selectedTask = task;
+        addSubTask();
+    });
+
+    if (quill) {
+        pageModal.style.display = 'none';
+    }
+
+    const subtaskList = document.getElementById('subtask-list');
+    subtaskList.innerHTML = ''; 
+
+    if (task.subTask && task.subTask.length > 0) 
+    {
+        task.subTask.forEach((subtask) => 
+        {
+            const subtaskItem = document.createElement('li');
+            subtaskItem.textContent = subtask.title;
+
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+            deleteButton.classList.add('delete-button');
+
+            deleteButton.addEventListener('click', () => 
+            {
+                const index = task.subTask.indexOf(subtask);
+                if (index !== -1) 
+                {
+                    task.subTask.splice(index, 1);
+                    renderTaskDetails(task); 
+                }
+            });
+
+            subtaskItem.appendChild(deleteButton);
+            subtaskList.appendChild(subtaskItem);
+        });
+    }
+
     taskDetails.appendChild(title);
     taskDetails.appendChild(textBody);
     taskDetails.appendChild(editButton);
-    taskDetails.appendChild(deleteButton);
-    taskDetails.appendChild(subPageButton);
-
+    taskDetails.appendChild(addSubTaskButton);
 }
+
+function addSubTask()
+{
+    if(selectedTask == null)
+    return;
+
+    const subTaskForm = document.getElementById('subtask-form');
+    subTaskForm.style.display = 'block';
+
+    taskDetails.innerHTML = '';
+    
+    const subtaskTitle = document.getElementById('subtask-title');
+    const subtaskText = document.getElementById('subtask-text');
+    const saveSubtaskButton = document.getElementById('save-subtask-btn');
+
+    saveSubtaskButton.addEventListener('click', () =>
+    {
+        const title = subtaskTitle.value;
+        const text = subtaskText.value;
+
+        const subTask = 
+        {
+            title : title,
+            text : text,
+        }
+        console.log(subTask);
+
+        if(!selectedTask.subTask)
+        selectedTask.subTask = [];
+
+        selectedTask.subTask.push(subTask);
+        
+        let existingTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        const taskIndex = existingTasks.findIndex(task => task.title === selectedTask.title);
+        existingTasks[taskIndex] = selectedTask;
+        localStorage.setItem('tasks', JSON.stringify(existingTasks));
+
+        pageTitleInput.value = '';
+        pageText.value = '';
+        subTaskForm.style.display = 'none';
+        renderTaskDetails(selectedTask);
+    }); 
+}
+
 
 window.addEventListener('load', () => 
 {
@@ -105,7 +187,6 @@ window.addEventListener('load', () =>
 
 
 
- 
 
 
 
@@ -114,156 +195,3 @@ window.addEventListener('load', () =>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    function createTask() {
-        pageModal.style.display = 'block';
-        savePageBtn.textContent = 'Save';
-        cancelEditBtn.style.display = 'none';
-        selectedIndex = null;
-        pageTitleInput.value = '';
-        pageTextInput.value = '';
-    }
-
-document.addEventListener('DOMContentLoaded', () => {
-    const pageList = document.getElementById('page-list');
-    const pageContent = document.getElementById('page-content');
-    const createPageBtn = document.getElementById('create-page-btn');
-    const pageModal = document.getElementById('page-modal');
-    const pageTitleInput = document.getElementById('page-title');
-    const pageTextInput = document.getElementById('page-text');
-    const savePageBtn = document.getElementById('save-page-btn');
-    const editPageBtn = document.getElementById('edit-page-btn');
-    const cancelEditBtn = document.getElementById('cancel-edit-btn');
-
-    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    let selectedIndex = null;
-
-    function saveTasksToLocalStorage() {
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
-
-    function renderTasks() {
-        pageList.innerHTML = '';
-        tasks.forEach((task, index) => {
-            const listItem = document.createElement('li');
-            listItem.textContent = task.title;
-
-
-            listItem.addEventListener('click', () => {
-                renderTaskContent(index);
-                selectedIndex = index;
-                editPageBtn.style.display = 'block';
-            });
-
-
-            const deleteBtn = document.createElement('button');
-            deleteBtn.textContent = 'Delete';
-            deleteBtn.classList.add('delete-button'); 
-            deleteBtn.addEventListener('click', (event) => {
-                deleteTask(index);
-            });
-
-            const editBtn = document.createElement('button');
-            editBtn.textContent = 'Edit';
-            editBtn.addEventListener('click', () => {
-                editTask(index);
-            });
-
-            listItem.appendChild(deleteBtn);
-            listItem.appendChild(editBtn); 
-            pageList.appendChild(listItem);
-        });
-    }
-
-    function renderTaskContent(index) {
-        pageContent.innerHTML = '';
-        const task = tasks[index];
-
-        const titleElement = document.createElement('h2');
-        titleElement.textContent = task.title;
-
-        const textElement = document.createElement('p');
-        textElement.textContent = task.text;
-
-        pageContent.appendChild(titleElement);
-        pageContent.appendChild(textElement);
-    }
-
-    
-
-    function saveTask() {
-        const title = pageTitleInput.value;
-        const text = pageTextInput.value;
-
-        if (selectedIndex !== null) {
-            tasks[selectedIndex].title = title;
-            tasks[selectedIndex].text = text;
-            savePageBtn.textContent = 'Save';
-            cancelEditBtn.style.display = 'none';
-            selectedIndex = null;
-        } else {
-            tasks.push({ title, text });
-        }
-
-        saveTasksToLocalStorage();
-        renderTasks();
-        renderTaskContent(tasks.length - 1);
-        pageModal.style.display = 'none';
-    }
-
-    function deleteTask(index) {
-        tasks.splice(index, 1);
-        saveTasksToLocalStorage();
-        renderTasks();
-        pageContent.innerHTML = '';
-    }
-
-    function editTask(index) {
-        pageModal.style.display = 'block';
-        const task = tasks[index];
-        pageTitleInput.value = task.title;
-        pageTextInput.value = task.text;
-        savePageBtn.textContent = 'Update';
-        cancelEditBtn.style.display = 'block';
-    }
-
-    cancelEditBtn.addEventListener('click', () => {
-        pageModal.style.display = 'none';
-        selectedIndex = null;
-        pageTitleInput.value = '';
-        pageTextInput.value = '';
-        savePageBtn.textContent = 'Save';
-        cancelEditBtn.style.display = 'none';
-    });
-
-    createPageBtn.addEventListener('click', createTask);
-    savePageBtn.addEventListener('click', saveTask);
-    editPageBtn.addEventListener('click', () => {
-        if (selectedIndex !== null) {
-            pageModal.style.display = 'block';
-            pageTitleInput.value = tasks[selectedIndex].title;
-            pageTextInput.value = tasks[selectedIndex].text;
-            savePageBtn.textContent = 'Update';
-            cancelEditBtn.style.display = 'block';
-        }
-    });     
-    renderTasks();
-});
-
-*/
