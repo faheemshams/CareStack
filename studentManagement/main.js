a2043e2da45f6d8114ff78d4b7669c4dc8b0f7fd
faheemshams <faheemshams58@gmail.com>
2023-09-10 23:08:57 +0530

    final

--- studentManagement/main.js
+++ studentManagement/main.js
@@ -3,9 +3,9 @@ const modal = document.getElementById('myModal');
 const closeModalBtn = document.getElementById('closeModalBtn');
 const modalCancelButton = document.getElementById('cancelStudentBtn');
 const modalSaveButton = document.getElementById('saveStudentBtn');
-
-
-
+const deleteModal = document.getElementById('deleteModal');
+const confirmDeleteBtn = document.getElementById('confirmDeleteBtn');
+const cancelDeleteBtn = document.getElementById('cancelDeleteBtn');
 
 addStudentButton.addEventListener('click', ()=>
 {
@@ -42,12 +42,7 @@ modalSaveButton.addEventListener('click', ()=>
 
 function addStudentToDB(student) 
 {
-    const indexedDB =
-        window.indexedDB ||
-        window.mozIndexedDB ||
-        window.webkitIndexedDB ||
-        window.msIndexedDB ||
-        window.shimIndexedDB;
+    const indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
 
     if (!indexedDB) {
         console.log("IndexedDB could not be found in this browser.");
@@ -79,9 +74,149 @@ function addStudentToDB(student)
             console.log("Student added to the database");
         };
     };
+    fetchStudentsFromDB();
 }
 
 function generateUniqueId() 
 {
     return Date.now();
 }
+
+function fetchStudentsFromDB() 
+{
+    const indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+
+    if (!indexedDB) 
+    {
+        console.log("IndexedDB could not be found in this browser.");
+        return;
+    }
+
+    const request = indexedDB.open("studentDb", 1);
+
+    request.onerror = function (event) {
+        console.error("An error occurred with IndexedDB");
+        console.error(event);
+    };
+
+    request.onsuccess = function () {
+        const db = request.result;
+        const transaction = db.transaction("students", "readonly");
+        const store = transaction.objectStore("students");
+
+        const students = [];
+
+        store.openCursor().onsuccess = function (event) {
+            const cursor = event.target.result;
+            if (cursor) 
+            {
+                students.push(cursor.value);
+                cursor.continue();
+            } 
+            else 
+            {
+                renderStudentsTable(students);
+                db.close();
+            }
+        };
+    };
+}
+
+function renderStudentsTable(students) 
+{
+    const tableBody = document.getElementById('studentTableBody');
+    tableBody.innerHTML = ''; 
+
+    students.forEach((student) => {
+        const row = document.createElement('tr');
+
+        const nameCell = document.createElement('td');
+        nameCell.textContent = student.name;
+
+        const ageCell = document.createElement('td');
+        ageCell.textContent = student.age;
+
+        const classCell = document.createElement('td');
+        classCell.textContent = student.class;
+
+        const addressCell = document.createElement('td');
+        addressCell.textContent = student.address;
+
+        const deleteCell = document.createElement('td');
+        const deleteButton = document.createElement('button');
+        deleteButton.style.background = '#d9534f';
+        deleteButton.style.color = '#fff';
+        deleteButton.style.borderRadius = '5px';
+
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => 
+        {
+            showDeleteModal(student.id);
+        });
+
+        deleteCell.appendChild(deleteButton);
+
+        row.appendChild(nameCell);
+        row.appendChild(ageCell);
+        row.appendChild(classCell);
+        row.appendChild(addressCell);
+        row.appendChild(deleteCell);
+
+        tableBody.appendChild(row);
+    });
+}
+
+function showDeleteModal(studentId) 
+{
+    deleteModal.style.display = 'block';
+    confirmDeleteBtn.onclick = function () 
+    {
+        deleteStudent(studentId);
+        deleteModal.style.display = 'none';
+    };
+
+    cancelDeleteBtn.onclick = function () 
+    {
+        deleteModal.style.display = 'none';
+    };
+}
+
+function deleteStudent(studentId) {
+    const indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+
+    if (!indexedDB) {
+        console.log("IndexedDB could not be found in this browser.");
+        return;
+    }
+
+    const request = indexedDB.open("studentDb", 1);
+
+    request.onerror = function (event) {
+        console.error("An error occurred with IndexedDB");
+        console.error(event);
+    };
+
+    request.onsuccess = function () {
+        const db = request.result;
+        const transaction = db.transaction("students", "readwrite");
+        const store = transaction.objectStore("students");
+
+        const deleteRequest = store.delete(studentId);
+
+        deleteRequest.onsuccess = function () {
+            console.log(`Student with ID ${studentId} deleted from the database`);
+            fetchStudentsFromDB(); // Refresh the table after deletion
+        };
+
+        deleteRequest.onerror = function (event) {
+            console.error(`Error deleting student with ID ${studentId}:`, event.target.error);
+        };
+
+        transaction.oncomplete = function () {
+            db.close();
+        };
+    };
+}
+
+
+fetchStudentsFromDB();
